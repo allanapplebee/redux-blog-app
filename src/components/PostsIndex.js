f5150fc1d3833527963ed98c02e7bed497123734
Gaby Saunders <allan.applebee@gmail.com>
2018-08-19 22:44:02 +1000

    mainly setup work

---
+++ src/components/PostsIndex.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+import _ from 'lodash';
+
+class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, posts => {
+      return <li className="list-group-item" key="post.id">
+        {posts.title}
+      </li>
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+//can pass in action creator directly, so no need for mapDispatch function here
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
